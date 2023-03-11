b4bb2727f43a3209f0495bf2195f33aaac477443
Nai-12 <nabail349@gmail.com>
2023-03-11 19:49:16 +0700

    Delete blog directory

--- blog/blog js/blogmotor.js
+++
@@ -1,6 +0,0 @@
-const header = document.querySelector(".bars");
-const navList = document.querySelector("nav ul");
-
-header.addEventListener('click', function () {
-    navList.classList.toggle('slide');
-})
